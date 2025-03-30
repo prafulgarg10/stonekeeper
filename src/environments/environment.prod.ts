59bfd6ffc4de872a760db7c3654eb5dca411b65d
Praful Garg <prafulgarg10@Prafuls-MacBook-Air.local>
2025-03-30 22:42:12 +0530

    updated url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: ''  //api url for prod env
+    apiUrl: 'http://ec2-52-66-29-30.ap-south-1.compute.amazonaws.com:3001'  //api url for prod env
   };
